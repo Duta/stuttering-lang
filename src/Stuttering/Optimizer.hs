c50db4c2af6fae766f159667f8c40b089e4b9982
Duta <wheen.b@gmail.com>
2014-04-13 20:57:06 +0100

    Extended basic optimization model

--- src/Stuttering/Optimizer.hs
+++ src/Stuttering/Optimizer.hs
@@ -5,8 +5,23 @@ module Stuttering.Optimizer
 import Stuttering.Parser
 
 optimize :: Stmt -> Stmt
-optimize (Seq stmts)       = Seq stmts
-optimize (Assign id expr)  = Assign id expr
-optimize (Print expr)      = Print expr
-optimize (If cond s1 s2)   = If cond s1 s2
-optimize (While cond stmt) = While cond stmt
+optimize = optStmt
+
+optStmt :: Stmt -> Stmt
+optStmt (Seq stmts)       = Seq (map optStmt stmts)
+optStmt (Assign id expr)  = Assign id (optAExpr expr)
+optStmt (Print expr)      = Print (optAExpr expr)
+optStmt (If cond s1 s2)   = If (optBExpr cond) (optStmt s1) (optStmt s2)
+optStmt (While cond stmt) = While (optBExpr cond) (optStmt stmt)
+
+optAExpr :: AExpr -> AExpr
+optAExpr (Var str)          = Var str
+optAExpr (IntConst int)     = IntConst int
+optAExpr (Neg expr)         = Neg (optAExpr expr)
+optAExpr (ABinary op e1 e2) = ABinary op (optAExpr e1) (optAExpr e2)
+
+optBExpr :: BExpr -> BExpr
+optBExpr (BoolConst bool)   = BoolConst bool
+optBExpr (Not expr)         = Not (optBExpr expr)
+optBExpr (BBinary op e1 e2) = BBinary op (optBExpr e1) (optBExpr e2)
+optBExpr (RBinary op e1 e2) = RBinary op (optAExpr e1) (optAExpr e2)
