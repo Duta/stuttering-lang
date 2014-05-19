cf127e410f2140eb7be5cf13567bb45475027d91
Bertie Wheen <wheen.b@gmail.com>
2014-05-19 22:19:58 +0100

    Updated optimizer to reflect changes
    Just realized the operator precedence is all mucked up

--- src/Stuttering/Optimizer.hs
+++ src/Stuttering/Optimizer.hs
@@ -2,58 +2,46 @@ module Stuttering.Optimizer
 ( optimize
 ) where
 
-import Stuttering.Parser
+import Stuttering.AST
 
 optimize :: Stmt -> Stmt
 optimize = cfStmt
 
--- Perform constant folding on a statement
 cfStmt :: Stmt -> Stmt
-cfStmt (Seq stmts)       = Seq (map cfStmt stmts)
-cfStmt (Assign id expr)  = Assign id (cfAExpr expr)
-cfStmt (Print expr)      = Print (cfAExpr expr)
-cfStmt (If cond s1 s2)   = case (cfBExpr cond, cfStmt s1, cfStmt s2) of
-  (BoolConst True,  os1, _) -> os1
-  (BoolConst False, _, os2) -> os2
-  (cfCond, os1, os2)       -> If cfCond os1 os2
-cfStmt (While cond stmt) = While (cfBExpr cond) (cfStmt stmt)
+cfStmt (Seq stmts)         = Seq $ map cfStmt stmts
+cfStmt (Assign ident expr) = Assign ident (cfExpr expr)
+cfStmt (Print expr)        = Print (cfExpr expr)
+cfStmt (While cond stmt)   = While (cfExpr cond) (cfStmt stmt)
+cfStmt (If cond s1 s2)     = case (cfExpr cond, cfStmt s1, cfStmt s2) of
+  (BoolConst True,  optS1, _) -> optS1
+  (BoolConst False, _, optS2) -> optS2
+  (optCond, optS1, optS2)     -> If optCond optS1 optS2
 
--- Perform constant folding on a AExpr
-cfAExpr :: AExpr -> AExpr
-cfAExpr (Var str)          = Var str
-cfAExpr (IntConst int)     = IntConst int
-cfAExpr (Neg expr)         = case cfAExpr expr of
-  IntConst int -> IntConst $ -int
-  cfExpr      -> Neg cfExpr
-cfAExpr (ABinary op e1 e2) = case (cfAExpr e1, cfAExpr e2) of
-  (IntConst i1, IntConst i2) -> IntConst $ case op of
-    Add      -> i1 + i2
-    Subtract -> i1 - i2
-    Multiply -> i1 * i2
-    Divide   -> i1 `div` i2
-  (oe1, oe2)                 -> ABinary op oe1 oe2
-
--- Perform constant folding on a BExpr
-cfBExpr :: BExpr -> BExpr
-cfBExpr (BoolConst bool)    = BoolConst bool
-cfBExpr (Not expr)          = Not (cfBExpr expr)
-cfBExpr (BBinary And e1 e2) = case (cfBExpr e1, cfBExpr e2) of
-  (BoolConst True, x)  -> x
-  (x, BoolConst True)  -> x
-  (BoolConst False, _) -> BoolConst False
-  (_, BoolConst False) -> BoolConst False
-  (oe1, oe2)           -> BBinary And oe1 oe2
-cfBExpr (BBinary Or e1 e2)  = case (cfBExpr e1, cfBExpr e2) of
-  (BoolConst False, x) -> x
-  (x, BoolConst False) -> x
-  (BoolConst True, _)  -> BoolConst True
-  (_, BoolConst True)  -> BoolConst True
-  (oe1, oe2)           -> BBinary And oe1 oe2
-cfBExpr (RBinary op e1 e2)  = case (cfAExpr e1, cfAExpr e2) of
-  (IntConst i1, IntConst i2) -> BoolConst $ case op of
-    Greater        -> i1 >  i2
-    Less           -> i1 <  i2
-    Equal          -> i1 == i2
-    GreaterOrEqual -> i1 >= i2
-    LessOrEqual    -> i1 <= i2
-  (oe1, oe2)                 -> RBinary op oe1 oe2
+cfExpr :: Expr -> Expr
+cfExpr (Var ident) = Var ident
+cfExpr (IntConst int) = IntConst int
+cfExpr (BoolConst bool) = BoolConst bool
+cfExpr (UnaryOp op expr) = case (op, cfExpr expr) of
+  (Negate, IntConst int) -> IntConst $ -int
+  (Not, BoolConst bool)  -> BoolConst $ not bool
+  (_, optExpr)           -> UnaryOp op optExpr
+cfExpr (BinaryOp op e1 e2) = case (op, cfExpr e1, cfExpr e2) of
+  (Add, IntConst int1, IntConst int2)            -> IntConst $ int1 + int2
+  (Subtract, IntConst int1, IntConst int2)       -> IntConst $ int1 - int2
+  (Multiply, IntConst int1, IntConst int2)       -> IntConst $ int1 * int2
+  (Divide, IntConst int1, IntConst int2)         -> IntConst $ int1 `div` int2
+  (And, optE1, BoolConst True)                   -> optE1
+  (And, BoolConst True, optE2)                   -> optE2
+  (And, optE1, BoolConst False)                  -> BoolConst False
+  (And, BoolConst False, optE2)                  -> BoolConst False
+  (Or, optE1, BoolConst True)                    -> BoolConst True
+  (Or, BoolConst True, optE2)                    -> BoolConst True
+  (Or, optE1, BoolConst False)                   -> optE1
+  (Or, BoolConst False, optE2)                   -> optE2
+  (Greater, IntConst int1, IntConst int2)        -> BoolConst $ int1 > int2
+  (Less, IntConst int1, IntConst int2)           -> BoolConst $ int1 < int2
+  (GreaterOrEqual, IntConst int1, IntConst int2) -> BoolConst $ int1 >= int2
+  (LessOrEqual, IntConst int1, IntConst int2)    -> BoolConst $ int1 <= int2
+  (Equal, IntConst int1, IntConst int2)          -> BoolConst $ int1 == int2
+  (Equal, BoolConst bool1, BoolConst bool2)      -> BoolConst $ bool1 == bool2
+  (_, optE1, optE2)                              -> BinaryOp op optE1 optE2
