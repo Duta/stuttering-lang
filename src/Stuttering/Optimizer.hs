946904dd9089935ef109f3dea911c1e21c6119c6
Bertie Wheen <wheen.b@gmail.com>
2014-05-19 19:24:22 +0100

    More extensible optimization function names

--- src/Stuttering/Optimizer.hs
+++ src/Stuttering/Optimizer.hs
@@ -5,25 +5,27 @@ module Stuttering.Optimizer
 import Stuttering.Parser
 
 optimize :: Stmt -> Stmt
-optimize = optStmt
+optimize = cfStmt
 
-optStmt :: Stmt -> Stmt
-optStmt (Seq stmts)       = Seq (map optStmt stmts)
-optStmt (Assign id expr)  = Assign id (optAExpr expr)
-optStmt (Print expr)      = Print (optAExpr expr)
-optStmt (If cond s1 s2)   = case (optBExpr cond, optStmt s1, optStmt s2) of
+-- Perform constant folding on a statement
+cfStmt :: Stmt -> Stmt
+cfStmt (Seq stmts)       = Seq (map cfStmt stmts)
+cfStmt (Assign id expr)  = Assign id (cfAExpr expr)
+cfStmt (Print expr)      = Print (cfAExpr expr)
+cfStmt (If cond s1 s2)   = case (cfBExpr cond, cfStmt s1, cfStmt s2) of
   (BoolConst True,  os1, _) -> os1
   (BoolConst False, _, os2) -> os2
-  (optCond, os1, os2)       -> If optCond os1 os2
-optStmt (While cond stmt) = While (optBExpr cond) (optStmt stmt)
+  (cfCond, os1, os2)       -> If cfCond os1 os2
+cfStmt (While cond stmt) = While (cfBExpr cond) (cfStmt stmt)
 
-optAExpr :: AExpr -> AExpr
-optAExpr (Var str)          = Var str
-optAExpr (IntConst int)     = IntConst int
-optAExpr (Neg expr)         = case optAExpr expr of
+-- Perform constant folding on a AExpr
+cfAExpr :: AExpr -> AExpr
+cfAExpr (Var str)          = Var str
+cfAExpr (IntConst int)     = IntConst int
+cfAExpr (Neg expr)         = case cfAExpr expr of
   IntConst int -> IntConst $ -int
-  optExpr      -> Neg optExpr
-optAExpr (ABinary op e1 e2) = case (optAExpr e1, optAExpr e2) of
+  cfExpr      -> Neg cfExpr
+cfAExpr (ABinary op e1 e2) = case (cfAExpr e1, cfAExpr e2) of
   (IntConst i1, IntConst i2) -> IntConst $ case op of
     Add      -> i1 + i2
     Subtract -> i1 - i2
@@ -31,22 +33,23 @@ optAExpr (ABinary op e1 e2) = case (optAExpr e1, optAExpr e2) of
     Divide   -> i1 `div` i2
   (oe1, oe2)                 -> ABinary op oe1 oe2
 
-optBExpr :: BExpr -> BExpr
-optBExpr (BoolConst bool)    = BoolConst bool
-optBExpr (Not expr)          = Not (optBExpr expr)
-optBExpr (BBinary And e1 e2) = case (optBExpr e1, optBExpr e2) of
+-- Perform constant folding on a BExpr
+cfBExpr :: BExpr -> BExpr
+cfBExpr (BoolConst bool)    = BoolConst bool
+cfBExpr (Not expr)          = Not (cfBExpr expr)
+cfBExpr (BBinary And e1 e2) = case (cfBExpr e1, cfBExpr e2) of
   (BoolConst True, x)  -> x
   (x, BoolConst True)  -> x
   (BoolConst False, _) -> BoolConst False
   (_, BoolConst False) -> BoolConst False
   (oe1, oe2)           -> BBinary And oe1 oe2
-optBExpr (BBinary Or e1 e2)  = case (optBExpr e1, optBExpr e2) of
+cfBExpr (BBinary Or e1 e2)  = case (cfBExpr e1, cfBExpr e2) of
   (BoolConst False, x) -> x
   (x, BoolConst False) -> x
   (BoolConst True, _)  -> BoolConst True
   (_, BoolConst True)  -> BoolConst True
   (oe1, oe2)           -> BBinary And oe1 oe2
-optBExpr (RBinary op e1 e2)  = case (optAExpr e1, optAExpr e2) of
+cfBExpr (RBinary op e1 e2)  = case (cfAExpr e1, cfAExpr e2) of
   (IntConst i1, IntConst i2) -> BoolConst $ case op of
     Greater        -> i1 >  i2
     Less           -> i1 <  i2
